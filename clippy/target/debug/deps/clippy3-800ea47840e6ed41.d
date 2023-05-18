befc5e5c2e2db8a61a74561660ac35380331a93e
Pan Yanqi <2841954631@qq.com>
2023-05-18 23:42:45 +0800

    my solution to rustling

---
+++ clippy/target/debug/deps/clippy3-800ea47840e6ed41.d
@@ -0,0 +1,8 @@
+/workspaces/rustlings/exercises/clippy/target/debug/deps/clippy3-800ea47840e6ed41.rmeta: clippy3.rs Cargo.toml
+
+/workspaces/rustlings/exercises/clippy/target/debug/deps/clippy3-800ea47840e6ed41.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
